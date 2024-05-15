11dca4a07ac73accb397a7ff9f9eb3ff0d54d957
Glenis Reyes <glenisrr@gmail.com>
2024-05-15 13:17:53 -0500

    solo order

--- src/PartsUnlimited.Models/OrderDetail.cs
+++ src/PartsUnlimited.Models/OrderDetail.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.2
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace PartsUnlimited.Models
